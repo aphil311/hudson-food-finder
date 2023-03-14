dcd9fa3a2c378dcb5a499dd6fcc198db4cdb7297
Aidan Phillips <41242437+aphil311@users.noreply.github.com>
2023-03-14 17:07:01 -0500

    flaskified

---
+++ runserver.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import wsgiref.simple_server as wss
+import application
+
+def main():
+    # handle arguments
+    parser = argparse.ArgumentParser(
+        description='The registrar application')
+    parser.add_argument('port', type=int,
+        help='the port at which the server should listen')
+    args = parser.parse_args()
+    port = args.port
+
+    try:
+        httpd = wss.make_server('0.0.0.0', port, application.app)
+        print('Listening on port', port, '...')
+        httpd.serve_forever()
+    except Exception as ex:
+        print(ex, file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
