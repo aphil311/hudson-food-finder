61ac37dae26e0ce8e56fb0e4b4b80dcd46b68d46
Aidan Phillips <41242437+aphil311@users.noreply.github.com>
2023-04-04 00:44:42 -0400

    search on sort property change

--- mobile-app/static/scripts/filter.js
+++ mobile-app/static/scripts/filter.js
@@ -41,6 +41,7 @@ function setup() {
   search();
   $("#filter-button").on("click", toggle);
   $("#search-bar").on("keyup", search);
+  $("input:radio[name ='sort']").on("change", search);
 }
 
 // runs setup when document is ready
