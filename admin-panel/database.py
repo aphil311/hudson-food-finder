253e985917ae995d66e43101ea2b48a4f4de6430
Aidan Phillips <41242437+aphil311@users.noreply.github.com>
2023-04-10 18:06:02 -0400

    reads csvs

--- admin-panel/database.py
+++ admin-panel/database.py
@@ -103,9 +103,14 @@ def find_offerings(filter):
 # Returns: 0 if successful, 1 if not
 #-----------------------------------------------------------------------
 def bulk_update(filename):
+    ORG_CUTOFF = 5
+    SERVICE_CUTOFF = 12
+    GROUP_CUTOFF = 13
     # Truncate tables so we can insert new data
+    # comment out for testing
     exe_stmt('TRUNCATE offerings RESTART IDENTITY CASCADE', ())
     exe_stmt('TRUNCATE org_ownership RESTART IDENTITY CASCADE', ())
+    exe_stmt('TRUNCATE organizations RESTART IDENTITY CASCADE', ())
     try:
         with open(filename, 'r', encoding='utf-8') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
@@ -113,31 +118,86 @@ def bulk_update(filename):
             for row in csv_reader:
                 # skip header row
                 if line_count != 0:
-                    inputs = []
-                    inputs2 = []
-                    # create new offering
-                    stmt_str = 'INSERT INTO offerings ('
-                    stmt_str += 'title, days_open, days_desc, '
-                    stmt_str += 'start_time, '
-                    stmt_str += 'end_time, init_date, close_date, '
-                    stmt_str += 'off_service, group_served, off_desc)'
-                    stmt_str += 'VALUES ('
-                    stmt_str += '%s, %s, %s, %s, %s, %s, %s, %s, %s, '
-                    stmt_str += '%s)'
-                    # relate organization to offering
-                    stmt_str2 = 'INSERT INTO org_ownership ('
-                    stmt_str2 += 'org_id, off_id) VALUES ('
-                    stmt_str2 += '%s, %s)'
-                    for i, item in enumerate(row):
-                        # use first col to find org_id
-                        if i == 0:
-                            inputs2.append(item)
-                            inputs2.append(line_count)
-                        else:
-                            inputs.append(str(item))
-                    # execute statements
-                    exe_stmt(stmt_str, inputs)
-                    exe_stmt(stmt_str2, inputs2)
+                    sel_orgid_inputs = []
+                    sel_orgid = 'SELECT org_id FROM organizations '
+                    sel_orgid += 'WHERE org_name = %s'
+                    for cell in row[:1]:
+                        sel_orgid_inputs.append(cell)
+                    res = query(sel_orgid, sel_orgid_inputs)
+                    # if organization not in database, add it, else
+                    # save the org_id
+                    if not res:
+                        ins_org = 'INSERT INTO organizations '
+                        ins_org += '(org_name, phone, website, street, '
+                        ins_org += 'zip_code) VALUES (%s, %s, %s, %s, '
+                        ins_org += '%s)'
+                        ins_org_inputs = []
+                        for cell in row[0:ORG_CUTOFF]:
+                            print(cell)
+                            ins_org_inputs.append(cell)
+                        exe_stmt(ins_org, ins_org_inputs)
+                        res = query(sel_orgid, sel_orgid_inputs)
+                    org_id = res[0][0]
+
+                    # get service and group ids from database
+                    sel_serviceid_inputs = []
+                    sel_serviceid_inputs.append(row[SERVICE_CUTOFF])
+                    sel_serviceid = 'SELECT service_id FROM services '
+                    sel_serviceid += 'WHERE service_type = %s'
+                    service_id = query(sel_serviceid,
+                        sel_serviceid_inputs)
+                    # if service not in database, add it
+                    if not service_id:
+                        ins_service = 'INSERT INTO services '
+                        ins_service += '(service_type) VALUES (%s)'
+                        exe_stmt(ins_service, sel_serviceid_inputs)
+                        service_id = query(sel_serviceid,
+                            sel_serviceid_inputs)
+
+                    service_id = service_id[0][0]
+                    
+                    # repeat for group
+                    sel_groupid_inputs = []
+                    sel_groupid_inputs.append(row[GROUP_CUTOFF])
+                    sel_groupid = 'SELECT group_id FROM people_groups '
+                    sel_groupid += 'WHERE people_group = %s'
+                    group_id = query(sel_groupid, sel_groupid_inputs)
+                    # if group not in database, add it
+                    if not group_id:
+                        ins_group = 'INSERT INTO people_groups '
+                        ins_group += '(people_group) VALUES (%s)'
+                        exe_stmt(ins_group, sel_groupid_inputs)
+                        group_id = query(sel_groupid,
+                            sel_groupid_inputs)
+                    
+                    group_id = group_id[0][0]
+                    
+                    # add offering to database
+                    ins_off = 'INSERT INTO offerings '
+                    ins_off += '(title, days_open, days_desc, '
+                    ins_off += 'start_time, end_time, init_date, '
+                    ins_off += 'close_date, off_service, '
+                    ins_off += 'group_served, off_desc)'
+                    ins_off += 'VALUES (%s, %s, %s, %s, %s, %s, %s, '
+                    ins_off += '%s, %s, %s)'
+                    ins_off_inputs = []
+                    for cell in row[ORG_CUTOFF:SERVICE_CUTOFF]:
+                        print(cell)
+                        ins_off_inputs.append(cell)
+                    ins_off_inputs.append(service_id)
+                    ins_off_inputs.append(group_id)
+                    for cell in row[GROUP_CUTOFF+1:]:
+                        ins_off_inputs.append(cell)
+                    exe_stmt(ins_off, ins_off_inputs)
+
+                    # match offering to organization
+                    ins_own = 'INSERT INTO org_ownership '
+                    ins_own += '(org_id, off_id) VALUES (%s, %s)'
+                    ins_own_inputs = []
+                    ins_own_inputs.append(org_id)
+                    ins_own_inputs.append(line_count)
+                    exe_stmt(ins_own, ins_own_inputs)
+                # go to next line in csv
                 line_count += 1
     except Exception as ex:
         print(ex, file=sys.stderr)
@@ -145,6 +205,5 @@ def bulk_update(filename):
     return 0
 
 if __name__ == '__main__':
-    # testing code
-    # find_offerings(('%', 'start_time'))
-    bulk_update('test.csv')
+    print('testing database.py')
+    bulk_update('test2.csv')
