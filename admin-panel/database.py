f73fc0313f4c58bf8f0d07ca31252573763241f3
Aidan Phillips <41242437+aphil311@users.noreply.github.com>
2023-05-04 22:52:32 -0400

    print default

--- admin-panel/database.py
+++ admin-panel/database.py
@@ -405,11 +405,13 @@ def bulk_update(filename):
                         row['Start Date'] = temp[2] + '-' + str(temp[0]).zfill(2) + '-' + str(temp[1]).zfill(2)
                     else:
                         row['Start Date'] = '1970-01-01'
+                        print('default')
                     if row.get('End Date'):
                         temp = row.get('End Date').split('/')
                         row['End Date'] = temp[2] + '-' + str(temp[0]).zfill(2) + '-' + str(temp[1]).zfill(2)
                     else:
                         row['End Date'] = '9999-12-31'
+                        print('default')
                     session.execute(ins_off_stmt, {
                         'title': row.get('Title'),
                         'days_open': row.get('Days'),
