d9ee4fdce759f29acafa3a41b01025340097f711
Aidan Phillips <41242437+aphil311@users.noreply.github.com>
2023-05-06 15:19:04 -0400

    clean auth

--- admin-panel/auth.py
+++ admin-panel/auth.py
@@ -4,10 +4,9 @@
 # auth.py
 # Author: Zain Ahmed, Yousef Amin, Bob Dondero
 # With lots of help from https://realpython.com/flask-google-login/
+# Helper functions to handle Google authentication.
 #-----------------------------------------------------------------------
 
-import os
-#import sys
 import json
 import flask
 import oauthlib
@@ -15,11 +14,9 @@ import oauthlib.oauth2
 import requests
 from decouple import config
 
-
-
 #-----------------------------------------------------------------------
-
-GOOGLE_DISCOVERY_URL = ('https://accounts.google.com/.well-known/openid-configuration')
+GOOGLE_DISCOVERY_URL = 'https://accounts.google.com/'
+GOOGLE_DISCOVERY_URL += '.well-known/openid-configuration'
 
 # change to env file after testing locally
 GOOGLE_CLIENT_ID = config('GOOGLE_CLIENT_ID')
@@ -28,19 +25,14 @@ GOOGLE_CLIENT_ID = config('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = config('GOOGLE_CLIENT_SECRET')
 
 client = oauthlib.oauth2.WebApplicationClient(GOOGLE_CLIENT_ID)
-
 #-----------------------------------------------------------------------
 
+# login() --------------------------------------------------------------
+# Log in to the application.
 def login():
-    
-    # Determine the URL for Google login.
-
-    print(GOOGLE_CLIENT_ID)
-    print(GOOGLE_CLIENT_SECRET)
-
     google_provider_cfg = requests.get(GOOGLE_DISCOVERY_URL).json()
     authorization_endpoint = (
-            google_provider_cfg['authorization_endpoint'])
+        google_provider_cfg['authorization_endpoint'])
 
     # Construct the request URL for Google login, providing scopes
     # to fetch the user's profile data.
@@ -48,20 +40,14 @@ def login():
         authorization_endpoint,
         redirect_uri = flask.request.base_url + '/callback',
         scope=['openid', 'email', 'profile'],
-        )
-
-    #-------------------------------------------------------------------
-    # For learning:
-    # print('request_uri:', request_uri, file=sys.stderr)
-    #-------------------------------------------------------------------
+    )
 
     # Redirect to the request URL.
     return flask.redirect(request_uri)
 
-    #-----------------------------------------------------------------------
-
+# callback() -----------------------------------------------------------
+# Handle the Google redirect.
 def callback():
-
     # Get the authorization code that Google sent.
     code = flask.request.args.get('code')
 
@@ -94,7 +80,6 @@ def callback():
     # including the user's Google profile image and email address.
     userinfo_endpoint = google_provider_cfg['userinfo_endpoint']
     uri, headers, body = client.add_token(userinfo_endpoint)
-
     userinfo_response = requests.get(uri, headers=headers, data=body)
 
     # Optional: Make sure the user's email address is verified.
@@ -103,7 +88,6 @@ def callback():
         return message, 400
 
     # Save the user profile data in the session.
-
     flask.session['sub'] = userinfo_response.json()['sub']
     flask.session['name'] = userinfo_response.json()['name']
     flask.session['given_name'] = userinfo_response.json()['given_name']
@@ -114,30 +98,21 @@ def callback():
     flask.session['email_verified'] = (
     userinfo_response.json()['email_verified'])
     flask.session['locale'] = userinfo_response.json()['locale']
+
+    # Redirect to the main application page.
     return flask.redirect(flask.url_for('index'))
 
-#-----------------------------------------------------------------------
-
+# logoutapp() ----------------------------------------------------------
+# Log out of the application.
 def logoutapp():
-    
     # Log out of the application.
     flask.session.clear()
     html_code = flask.render_template('loggedout.html')
     response = flask.make_response(html_code)
     return response
 
-#-----------------------------------------------------------------------
-
-def logoutgoogle():
-
-    # Log out of the application.
-    flask.session.clear()
-    # Log out of Google.
-    flask.abort(flask.redirect(
-        'https://mail.google.com/mail/u/0/?logout&hl=en'))
-
-#-----------------------------------------------------------------------
-
+# authenticate() -------------------------------------------------------
+# Return the user's email address if the user is logged in.
 def authenticate():
     if 'email' not in flask.session:
         flask.abort(flask.redirect(flask.url_for('login')))
