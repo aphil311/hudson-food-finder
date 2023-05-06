799b74cd2e855b0b489077abdf613a3988c2442f
Aidan Phillips <41242437+aphil311@users.noreply.github.com>
2023-05-06 13:06:48 -0400

    config

--- admin-panel/config/init.py
+++ admin-panel/config/init.py
@@ -1,3 +1,8 @@
+#-----------------------------------------------------------------------
+# This file is used to initialize the flask app and the database
+# engine. It also sets the path to the templates and static folders.
+#-----------------------------------------------------------------------
+
 import os
 import flask
 import sqlalchemy
@@ -5,9 +10,12 @@ import psycopg2
 from decouple import config
 from config.definitions import ROOT_DIR
 
+# get paths
 templates = os.path.join(ROOT_DIR, 'templates')
 static = os.path.join(ROOT_DIR, 'static')
 _DATABASE_URL_ = config('DB_URL')
+
+# initialize flask app and database engine
 app = flask.Flask(__name__, template_folder=templates,
         static_folder=static)
 engine = sqlalchemy.create_engine('postgresql://',
